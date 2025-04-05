21db06bb865791fbef564b0eaa450e8974414f53
Gudla Dinesh Kumar <132870643+DjDinesh1234@users.noreply.github.com>
2025-04-05 13:24:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,13 @@
 provider "aws" {
-region = "ap-south-1"
-
+  region     = "us-east-1"
+  access_key = "AKIAYHHGE4VOUPDHLTIR"
+  secret_key = "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"
 }
 
 resource "aws_instance" "key" {
-ami = "ami-012b9156f755804f5"
+ami = "ami-00a929b66ed6e0de6"
 instance_type = "t2.micro"
-key_name = "nwkpp"
+key_name = "jenkinskey"
 count = 1
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
